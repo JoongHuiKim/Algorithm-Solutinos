b6ca6098b312639ff11242454a6bbfa52475e235
Joong Hui Kim <ire917@gmail.com>
2022-04-18 16:34:03 +0900

    백준 BOJ17070 PyPy3 로 정답

--- BOJ17070.py
+++ BOJ17070.py
@@ -1,20 +1,35 @@
 #갈수있는 방향 0,1  1,1  1,0 3개만 가능
 #이동할때마다 이전행의 +1해서 N,M이 0이 아니면 그 값
+import sys
 
 N = int(input())
 data = []
 answer = 0
 for i in range(N):
-    data.append(list(map(int,input().split())))
+    data.append(list(map(int, sys.stdin.readline().split())))
+    #data.append(list(map(int,input().split())))
 
 
 def dfs(direction,x,y):
+    if data[N-1][N-1] == 1 or (data[N-1][N-2]==1 and data[N-2][N-1] == 1):
+        return
+
     global answer
 
     if x<0 or x>=N or y<0 or y>=N:
         return
     else:
         if x==N-1 and y==N-1:
+            if direction == "R":
+                if data[x][y] == 1:
+                    return
+            elif direction == "RD":
+                if data[x-1][y] == 1 or data[x][y] == 1 or data[x][y-1] == 1:
+                    return
+            elif direction == "D":
+                if data[x][y] == 1:
+                    return
+
             answer += 1
 
         else:
